facfb2e09037b6e0f1b445311ec87aa32aa57a38
root <tomoe@icepp.s.u-tokyo.ac.jp>
2019-05-29 05:18:28 +0000

    Un update.

--- gridalert/util/hash.py
+++ gridalert/util/hash.py
@@ -1,8 +1,8 @@
 import hashlib
 
-def md5(cluster, host, date, data):
+def md5(cluster, host, date, service, data):
 
-    dat = cluster + host + date + data
+    dat = cluster + host + date + service + data
     hs = hs = hashlib.md5(dat.encode()).hexdigest()
 
     return hs
